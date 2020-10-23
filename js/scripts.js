dcc1932adb1c7fb432cd824683af23c8aa8da3ed
Tyler Sinks <tyler.sinks@gmail.com>
2020-10-23 14:56:10 -0400

    add div class name-fields to capture user name, add jquery selectors for user input capturing in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,20 @@
 /*
 To reset the appearance of the form (for instance, in the case of dynamic form submissions using AJAX), remove the .was-validated class from the <form> again after submission.
 */
+
+$(document).ready(function() {
+  $("form").submit(function() {
+    event.preventDefault();
+    const seriousNess = $("input:radio[name=career]:checked").val();
+    const controlVChance = $("input:radio[name=control]:checked").val();
+    const mbType = $("input:radio[name=mb-personality]:checked").val();
+    
+    $("#output").show();
+  })
+})
+
+$(document).ready(function() {
+  $("form").reset(function() {
+    
+  })
+})
