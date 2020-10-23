136e3e4e083b1f2fa37dbe5555c27e5887923ce4
Tyler Sinks <tyler.sinks@gmail.com>
2020-10-23 17:32:26 -0400

    fix grammar in output div, and most of branching to scripts.js, remove comments from scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,25 +6,37 @@ To reset the appearance of the form (for instance, in the case of dynamic form s
 $(document).ready(function() {
   $("form").submit(function() {
     event.preventDefault();
-    // Capture all input values from the questions
+    let outputLang = "Ruby";
     const seriousNess = $("input:radio[name=career]:checked").val();
     const controlVChance = $("input:radio[name=control]:checked").val();
     const mbType = $("input:radio[name=mb-personality]:checked").val();
-    const 
-
-    // Insert name into <span class="user-name-insert">
-
-
-
-    // Insert language suggested to learn into span class="result">
-
-
+    let movieTitle = $("input[type=checkbox]:checked").val();
+    const origLangLean = $("input:radio[name=lang-lean]:checked").val();
+    let fullUserName = $("input#firstName").val();
+    fullUserName += " "
+    fullUserName += $("input#lastName").val();
+    $("span.user-name-insert").text(fullUserName);
+    if (movieTitle === "The Wizard of Oz" || "Tom Cruise" || "rom-com") {
+      movieTitle = "milquetoast movie";
+      outputLang = "Python";
+    } else if (movieTitle.includes("Stanley") || movieTitle.includes("Graduate")) {
+      movieTitle = "classic American movie";
+      outputLang += " or some other completely different programming language";
+    } else {
+      movieTitle = "book";
+      outputLang = "JavaScript";
+    }
+    $("span.favorite-movie").text(movieTitle);
+    if (mbType.includes("E") || mbType.includes("J")) {
+      outputLang = "JavaScript";   
+    }
+    $("span.result").text(outputLang);
     $("#output").show();
   })
 })
 
 $(document).ready(function() {
   $("form").reset(function() {
-
+    event.preventDefault();
   })
 })
