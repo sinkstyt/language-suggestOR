db7b75bc6f6523816e79a26346299ead2f7be86e
Tyler Sinks <tyler.sinks@gmail.com>
2020-10-23 15:17:46 -0400

    add img directory with one image per language, img element above h2 Instructions, and more scripts for user input capture

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,25 @@ To reset the appearance of the form (for instance, in the case of dynamic form s
 $(document).ready(function() {
   $("form").submit(function() {
     event.preventDefault();
+    // Capture all input values from the questions
     const seriousNess = $("input:radio[name=career]:checked").val();
     const controlVChance = $("input:radio[name=control]:checked").val();
     const mbType = $("input:radio[name=mb-personality]:checked").val();
-    
+    const 
+
+    // Insert name into <span class="user-name-insert">
+
+
+
+    // Insert language suggested to learn into span class="result">
+
+
     $("#output").show();
   })
 })
 
 $(document).ready(function() {
   $("form").reset(function() {
-    
+
   })
 })
