715f991b70bc9bb65ccd4473745aae5ba62c8707
Tyler Sinks <tyler.sinks@gmail.com>
2020-10-23 13:39:27 -0400

    add commented note for scripts.js guidelines, question 5, div wrappers for each question, and missing form opening element

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+// From getbootstrap.com documentation on form validation:
+/*
+To reset the appearance of the form (for instance, in the case of dynamic form submissions using AJAX), remove the .was-validated class from the <form> again after submission.
+*/
